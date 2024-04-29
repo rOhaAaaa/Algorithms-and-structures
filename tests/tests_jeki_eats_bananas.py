003f24b3e33920b56484da721d3380196b1488e4
rOhaAaaa <151580376+rOhaAaaa@users.noreply.github.com>
2024-04-29 18:51:30 +0300

    Update tests_jeki_eats_bananas.py

--- tests/tests_jeki_eats_bananas.py
+++ tests/tests_jeki_eats_bananas.py
@@ -1,4 +1,11 @@
 import unittest
+import os
+import sys
+test_file_path = os.path.abspath(__file__)
+common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
+src_path = os.path.join(common_parent_path, 'src')
+sys.path.append(src_path)
+from jeki_eats_bananas import min_eating_speed
 
 class TestBananaEatingSpeed(unittest.TestCase):
 
