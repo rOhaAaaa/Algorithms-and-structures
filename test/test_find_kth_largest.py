c6ed9dae4aec18be34fc92ebf5c09d535f9a7609
rOhaAaaa <151580376+rOhaAaaa@users.noreply.github.com>
2024-04-29 12:08:47 +0300

    Update test_find_kth_largest.py

--- test/test_find_kth_largest.py
+++ test/test_find_kth_largest.py
@@ -1,5 +1,7 @@
 import unittest
 
+from src.find_kth_largest import find_kth_largest
+
 class TestFindKthLargest(unittest.TestCase):
     def test_first_largest(self):
         self.assertEqual(find_kth_largest([3, 2, 1, 5, 6, 4], 1), 6, "Should be 6")
