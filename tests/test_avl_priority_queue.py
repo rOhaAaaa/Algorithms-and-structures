e21f8164b0c6833dbf330c3f4ebbb9e1ccc9d1ae
rOhaAaaa <151580376+rOhaAaaa@users.noreply.github.com>
2024-04-29 19:23:28 +0300

    Update test_avl_priority_queue.py

--- tests/test_avl_priority_queue.py
+++ tests/test_avl_priority_queue.py
@@ -5,8 +5,12 @@ test_file_path = os.path.abspath(__file__)
 common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
 src_path = os.path.join(common_parent_path, 'src')
 sys.path.append(src_path)
+<<<<<<< Updated upstream
 
 from avl_priority_queue import AVLPriorityQueue
+=======
+from avl_tree_priority_queue import AVLPriorityQueue
+>>>>>>> Stashed changes
 
 class TestAVLPriorityQueue(unittest.TestCase):
     def setUp(self):
