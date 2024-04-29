61c789ab1328629ba869b8c909cc3827b37879f9
rOhaAaaa <151580376+rOhaAaaa@users.noreply.github.com>
2024-04-29 19:14:41 +0300

    Update tests_binary_tree.py

--- tests/tests_binary_tree.py
+++ tests/tests_binary_tree.py
@@ -5,7 +5,7 @@ test_file_path = os.path.abspath(__file__)
 common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
 src_path = os.path.join(common_parent_path, 'src')
 sys.path.append(src_path)
-from invert_binary_tree import invert_binary_tree, BinaryTree
+from binary_tree import invert_binary_tree, BinaryTree
 
 class TestInvertBinaryTree(unittest.TestCase):
     def test_invert_binary_tree(self):
